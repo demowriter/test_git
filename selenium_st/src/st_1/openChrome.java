901792eac736a18f2cfda31bcfd4a382ce3d60d8
demowriter <674732928@qq.com>
2018-12-23 16:24:07 +0800

    12/23

--- selenium_st/src/st_1/openChrome.java
+++ selenium_st/src/st_1/openChrome.java
@@ -1,21 +1,34 @@
 package st_1;
 //
 import java.util.concurrent.TimeUnit;
-import java.util.*;
-
-import org.apache.xerces.util.SynchronizedSymbolTable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class openChrome {
+	private static String html;
+	private static String title;
+	private static String url;
+	private static WebElement divs;
+
 	//main方法参数必须为字符串数组(String [ ])，变量名可以随意，
 	//通常使用args即是arguments（”参数“的复数形式）的缩写。
+	public void wait(String[] args) {
+        
+		
+	}
 	public static void main(String[] args) {
 //        System.setProperty("webdriver.chrome.driver","E:\\webDriver\\chromedriverV2.28.exe");//chromedriver服务地址
         WebDriver driver =new ChromeDriver(); //新建一个WebDriver 的对象，但是new 的是FirefoxDriver的驱动
         driver.get("http://www.baidu.com");//打开指定的网站
         driver.findElement(By.id("kw")).sendKeys(new  String[] {"hello"});//找到kw元素的id，然后输入hello
         driver.findElement(By.id("su")).click(); //点击按扭
+        html=driver.getPageSource();
+        System.out.println("点击成功！");
+//        System.out.println(html);
+        title=driver.getTitle();
+        url=driver.getCurrentUrl();
         try {
             /**
              * WebDriver自带了一个智能等待的方法。
@@ -27,21 +40,32 @@ public class openChrome {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.printf("页面标题为: %s \n,网址为：%s \n",title,url);
+        
+       divs=driver.findElement(By.cssSelector
+    		   ("#\\31 > div.c-border > div.op_dict_content > "
+    		   		+ "table:nth-child(2) > tbody > tr > td:nth-child(2) "
+    		   		+ "> span"));
+        System.out.println(divs.getText());
+//        WebElement ele = driver.findElement(By.xpath("/html"));
+//        String htmlStr = ele.getAttribute("innerHTML");// 获取元素内容
+//        System.out.printf("%s \n %s",ele,htmlStr);
+        
+
         /**
          * dr.quit()和dr.close()都可以退出浏览器,简单的说一下两者的区别：第一个close，
          * 如果打开了多个页面是关不干净的，它只关闭当前的一个页面。第二个quit，
-         * 是退出了所有Webdriver所有的窗口，退的非常干净，所以推荐使用quit最为一个case退出的方法。
+         * 是退出了所有Webdriver所有的窗口，退的非常干净，所以推荐使用quit作为一个case退出的方法。
          */
         try {
-			Thread.sleep(50000);
+			Thread.sleep(5);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
        System.out.print("执行结束！");
        
-        driver.quit();//退出浏览器
+       driver.quit();//退出浏览器
     }
-	
 	 
 }
